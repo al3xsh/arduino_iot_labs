c6de9b5f40cc5ed560940a40f5eda4002a0fded9
Alex Shenfield <alex.shenfield@gmail.com>
2020-11-05 09:11:15 +0000

    added templates for MyCredentials.h

---
+++ lab_3/task_3/iot_light_switch/MyCredentials.h
@@ -0,0 +1,7 @@
+// my wifi credentials
+const char mySSID[] = "<your SSID>";
+const char myPSK[]  = "<your password>";
+
+// my adafruit io credentials
+const char userid[] = "<your adafruit io username>";
+const char apikey[] = "<your adafruit io key>";
